d6a7eab64926a53c3f159460e4313d4e98177630
Vladimir Davidovich <thy.ringo@gmail.com>
2025-12-10 13:10:14 +0300

    nice

--- Runtime/Unity.NodeApi.sh
+++ Runtime/Unity.NodeApi.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-dotnet ../../../../../../out/bin/Release/NodeApi.Generator/net10.0/osx-arm64/Microsoft.JavaScript.NodeApi.Generator.dll \
-  --assembly "../../../Library/ScriptAssemblies/Unity.NodeApi.dll" \
-  --reference "Microsoft.JavaScript.NodeApi.dll" \
-  --typedefs "Unity.NodeApi.d.ts"
+pushd ../../../Build_BackUpThisFolder_ButDontShipItWithYourGame/Managed/
+
+# enumerate all .dll files in the current directory and put them to semicolon-separated list
+refs=$(find . -name "*.dll" -type f -print0 | sed 's/.\//;/g')
+
+dotnet ~/Documents/node-api-dotnet/out/bin/Release/NodeApi.Generator/net10.0/osx-arm64/Microsoft.JavaScript.NodeApi.Generator.dll \
+  --assembly "Unity.NodeApi.dll" \
+  --reference "$refs" \
+  --typedefs "Unity.NodeApi.d.ts" \
+  --framework "netstandard2.1"
+
+popd
