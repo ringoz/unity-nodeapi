a351d4ec5a2a98d4d98a6ad842a60c4595eda071
Vladimir Davidovich <thy.ringo@gmail.com>
2025-12-21 21:03:37 +0300

    html meta

--- index-web.js
+++ index-web.js
@@ -1,17 +1,16 @@
-import { getDefaultContext } from './Runtime/include/emnapi.mjs';
-
 const { Module } = await createUnityInstance(document.querySelector("#unity-canvas"), {
   arguments: [],
   dataUrl: "Build.web.data",
   frameworkUrl: "Build.web.framework.js",
   codeUrl: "Build.web.wasm",
   streamingAssetsUrl: "StreamingAssets",
-  companyName: "DefaultCompany",
-  productName: "unode-module",
-  productVersion: "1.0",
+  productName: document.title,
+  companyName: document.querySelector("meta[name='author']").content,
+  productVersion: document.querySelector("meta[name='version']").content,
   // matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
   // devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
 });
 
+import { getDefaultContext } from './Runtime/include/emnapi.mjs';
 const exports = Module.emnapiInit({ context: getDefaultContext() });
 export default exports;
