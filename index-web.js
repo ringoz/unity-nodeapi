f84b779ca645ff6c61941f35071483b526986765
Vladimir Davidovich <thy.ringo@gmail.com>
2026-01-08 21:22:21 +0300

    import stuff from parent package.json

--- index-web.js
+++ index-web.js
@@ -1,12 +1,13 @@
+import { name as productName, author as companyName, version as productVersion } from '../../../package.json';
 const { Module } = await createUnityInstance(document.querySelector("#unity-canvas"), {
   arguments: [],
   dataUrl: "Build/Build.web.data",
   frameworkUrl: "Build/Build.web.framework.js",
   codeUrl: "Build/Build.web.wasm",
   streamingAssetsUrl: "StreamingAssets",
-  productName: document.title,
-  companyName: document.querySelector("meta[name='author']")?.content,
-  productVersion: document.querySelector("meta[name='version']")?.content,
+  productName,
+  companyName,
+  productVersion,
 });
 
 import { getDefaultContext } from './Runtime/include/emnapi.mjs';
